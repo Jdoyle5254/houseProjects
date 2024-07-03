3eb972c3a172fc561e417516e4ce0720803514d9
Jennifer Doyle <jdoyle2@instructors.2u.com>
2024-07-02 22:08:18 -0500

    added additional html and js files

--- assets/js/viewproject.js
+++ assets/js/viewproject.js
@@ -6,7 +6,7 @@ document.getElementById('mytaskform').addEventListener('submit',function(event) 
     event.preventDefault(); 
     
     var taskAdd = document.getElementById('mytask').value
-     
+ 
     // console.log(value)
     var li = document.createElement('li')
    
@@ -17,25 +17,11 @@ document.getElementById('mytaskform').addEventListener('submit',function(event) 
 
 })
 
-// taskbutton.add("click", function(){
-//     console.log('button works')
-// })
 
 
 
-// document.getElementById('myForm').addEventListener('submit', function(event) {
-//     event.preventDefault(); // Prevent the form from submitting
 
-//     var inputValue = document.getElementById('myInput').value; // Get the input value
-//     if (inputValue.trim() !== '') { // Check if the input is not empty
-//         var li = document.createElement('li'); // Create a new <li> element
-//         li.textContent = inputValue; // Set the text content to the input value
 
-//         document.getElementById('myList').appendChild(li); // Append the <li> to the <ul>
-
-//         document.getElementById('myInput').value = ''; // Clear the input field
-//     }
-// });
 
 
  
